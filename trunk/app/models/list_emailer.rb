59ae73f6bcd7097b207188fbac58f0862d0907f2
John Barton (joho) <jrbarton@gmail.com>
2007-05-24 00:31:10 +0000

    another email fix
    git-svn-id: svn+ssh://alltimetop5.com/var/www/apps/alltimetop5/repos@21 3994479a-ae2f-0410-82b0-dc2e942132c9

--- trunk/app/models/list_emailer.rb
+++ trunk/app/models/list_emailer.rb
@@ -1,10 +1,10 @@
 class ListEmailer < ActionMailer::Base
 
   def share(list, email_addresses)
-    subject    = "#{list.user.username} wants to share their alltimetop5 #{list.title} with you"
-    body       = { :list => list }
-    recipients = email_addresses
-    from       = 'lists@alltimetop5.com'
-    sent_on    = Time.now
+    subject    "#{list.user.username} wants to share their alltimetop5 #{list.title} with you"
+    body      :list => list
+    recipients email_addresses
+    from       'lists@alltimetop5.com'
+    sent_on    Time.now
   end
 end
