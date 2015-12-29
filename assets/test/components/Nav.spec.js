dc883ead8a144048327e97ae627adf8cf912faed
John Barton (joho) <jrbarton@gmail.com>
2015-12-30 09:00:07 +1100

    Get the Nav class loaded in test via not-insane import statement.

--- assets/test/components/Nav.spec.js
+++ assets/test/components/Nav.spec.js
@@ -2,6 +2,8 @@ import React from 'react';
 import should from 'should';
 import { createRenderer } from 'react-addons-test-utils';
 
+import Nav from 'components/Nav';
+
 describe('Nav', () => {
   it('has a h1 with the site name', () => {
     let renderer = createRenderer();
