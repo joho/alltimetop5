6f68861913a50b968d1c330ee2ee970b485c49d1
John Barton (joho) <jrbarton@gmail.com>
2016-01-29 09:54:18 +1100

    Really shitty rough create form.

--- assets/src/components/NewListPage.jsx
+++ assets/src/components/NewListPage.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
 export default class NewListPage extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            list: [
+                '',
+                '',
+                '',
+                '',
+                ''
+            ]
+        };
+    }
     render() {
-        return(<p>Hello, I would like to make a new list</p>);
+        return(
+            <form>
+                {[1,2,3,4,5].map(this.renderFormInput.bind(this))}
+                <button type="submit" onClick={this.submit.bind(this)}>
+                    Save!
+                </button>
+            </form>
+        );
+    }
+
+    renderFormInput(num) {
+        return(
+            <label key={num}>#{num}
+                <input value={this.state[num - 1]} onChange={this.handleListEntryChange(num)} />
+            </label>
+        );
+    }
+
+    handleListEntryChange(num) {
+        return (e) => {
+            // TODO clone?
+            let newList = this.state.list;
+            newList[num - 1] = e.target.value;
+            this.setState({list: newList});
+        };
+    }
+
+    submit(e) {
+        e.preventDefault();
+        console.log("NewList state", this.state);
     }
 }
